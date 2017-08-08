85dc4a41328b3b070980605b1a2f42a0e2d305a7
Matt Jennings <matt@mattjennings.net>
2017-08-08 13:00:35 -0700

    added minified bundle.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 var path = require('path');
+var webpack = require('webpack');
 
 var config = {
     entry: './js/app.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js' 
-    }
+        filename: 'bundle.min.js' 
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({minimize: true})
+    ]
 };
 
 module.exports = config;
