07c39dc7675951244c31a66c6fd39c9a553a60c5
Matt Jennings <matt@mattjennings.net>
2017-08-09 09:41:14 -0700

    updated configuration files

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
-var config = {
+const config = {
     entry: './js/app.js',
     output: {
         path: path.resolve(__dirname, 'build'),
+        // File below used for dev
+        // filename: 'bundle.js'
+        
+        // File below used for production
         filename: 'bundle.min.js' 
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin()
-    ]
+    }
+    // ,
+    // plugins: [
+    //     new webpack.optimize.UglifyJsPlugin()
+    // ]
 };
 
 module.exports = config;
